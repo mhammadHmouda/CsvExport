c47488a446905900acbf756209defd967ec18350
Mohammad <hmoudahamood@gmail.com>
2023-09-22 12:53:29 +0300

    Initiate csv export project.

---
+++ src/main/java/com/harri/training2/CsvExportApplication.java
@@ -0,0 +1,13 @@
+package com.harri.training2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harri/training2/CsvExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harri.training2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvExportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
